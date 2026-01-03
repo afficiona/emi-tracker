76efa4c852694f33a6e64da0a71e2c1dc1fb0d71
Aman Pandey <afficiona4web@gmail.com>
2026-01-03 19:05:02 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'export', // Enables static exports
-  basePath: '/emi-tracker', // Set the base path for your application
-  assetPrefix: '/emi-tracker/', // Set the asset prefix for static assets
-  images: {
-    unoptimized: true, // Disable image optimization as it requires a server
-  }
 };
